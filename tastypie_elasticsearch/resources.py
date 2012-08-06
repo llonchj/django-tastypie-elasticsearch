f163eec7d0ec177bb36befdd90f6fee6e76411a4
Jordi Llonch <llonchj@atcubic.com>
2012-08-06 19:52:27 +1000

    thrift as default connection and CRUD initial support

--- tastypie_elasticsearch/resources.py
+++ tastypie_elasticsearch/resources.py
@@ -14,15 +14,31 @@ class ESResource(Resource):
     """
     class Meta:
         es_server = getattr(settings, 
-            "ES_INDEX_SERVER", "http://127.0.0.1:9200/")
+            "ES_INDEX_SERVER", "127.0.0.1:9500")
         es_timeout = 20
         
-        allowed_methods = ['get']
+        allowed_methods = ('get', 'post', 'delete')
     
+    _es = None
+    def es__get(self):
+        if self._es is None:
+            self._es = pyes.ES(server=self._meta.es_server, 
+                timeout=self._meta.es_timeout)
+        return self._es
+    es = property(es__get)
+    
+    def full_dehydrate(self, bundle):
+        bundle.data.update(bundle.obj)
+        bundle.data["_id"] = bundle.obj.get_id()
+        return bundle
+    
+    def full_hydrate(self, bundle):
+        bundle.obj.update(bundle.data)
+        return bundle
+
     def get_resource_uri(self, bundle_or_obj=None):
         if bundle_or_obj is None:
             result = super(ESResource, self).get_resource_uri(bundle_or_obj)
-            print result
             return result
 
         kwargs = {
@@ -40,8 +56,6 @@ class ESResource(Resource):
         return self._build_reverse_url("api_dispatch_detail", kwargs=kwargs)
 
     def get_object_list(self, request):
-        es = pyes.ES(self._meta.es_server, timeout=self._meta.es_timeout)
-
         offset = int(request.GET.get("offset", 0))
         limit = int(request.GET.get("limit", self._meta.limit))
 
@@ -49,7 +63,7 @@ class ESResource(Resource):
         search = pyes.query.Search(
             query=query, start=offset, size=limit)
 
-        results = es.search(search, indices=self._meta.indices)
+        results = self.es.search(search, indices=self._meta.indices)
 
         return results
             
@@ -58,13 +72,37 @@ class ESResource(Resource):
         return self.get_object_list(request)
 
     def obj_get(self, request=None, **kwargs):
-        es = pyes.ES(self._meta.es_server, timeout=self._meta.es_timeout)
 
         offset = int(request.GET.get("offset", 20))
         limit = int(request.GET.get("limit", 20))
         id = kwargs.get(self._meta.detail_uri_name)
         search = pyes.query.IdsQuery(id)
-        results = es.search(search, indices=self._meta.indices)
+        results = self.es.search(search, indices=self._meta.indices)
         return results[0]
 
-        
+    def obj_create(self, bundle, request=None, **kwargs):
+        bundle.obj = dict(kwargs)
+        bundle = self.full_hydrate(bundle)
+        pk = kwargs.get("pk", None)
+
+        result = self.es.index(bundle.obj.to_dict(), id=pk)
+
+        return result
+    
+    def obj_update(self, bundle, request=None, **kwargs):
+        return self.obj_create(bundle, request, **kwargs)
+
+    #def obj_delete_list(self, request=None, **kwargs):
+    #    bucket = self._bucket()
+    #
+    #    for key in bucket.get_keys():
+    #        obj = bucket.get(key)
+    #        obj.delete()
+    
+    def obj_delete(self, request=None, **kwargs):
+        pk = kwargs.get("pk")
+        return self.es.delete(id=pk)
+    
+    def rollback(self, bundles):
+        pass
+    
