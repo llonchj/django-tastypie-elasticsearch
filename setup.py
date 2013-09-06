ec46292eef9ec2a0a7c57a0bc1dae9e6b57cb059
Jordi Llonch <llonchj@gmail.com>
2013-09-07 03:28:25 +1000

    fixed setup.py

--- setup.py
+++ setup.py
@@ -5,37 +5,31 @@ from setuptools import setup, find_packages
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.test_project.settings")
 
-if __name__ == '__main__':
-    setup(
-        name = 'django-tastypie-elasticsearch',
-        version = '0.3.0',
-        description = "ElasticSearch support for django-tastypie.",
-        long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
-        author = 'Jordi Llonch',
-        author_email = 'llonchj@gmail.com',
-        url = 'https://github.com/llonchj/django-tastypie-elasticsearch',
-        keywords = "REST RESTful tastypie pyes elasticsearch django",
-        license = 'AGPLv3',
-        packages = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
-        classifiers = (
-            'Development Status :: 3 - Alpha',
-            'Environment :: Web Environment',
-            'Intended Audience :: Developers',
-            'License :: OSI Approved :: GNU Affero General Public License v3',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python',
-            'Framework :: Django',
-        ),
-        zip_safe = True,
-        install_requires = (
-            'Django>=1.4',
-            'django-tastypie>=0.9.11',
-            'pyes>=0.19.1',
-        ),
-        test_suite = 'tests.runtests.runtests',
-        tests_require = (
-            'Django>=1.4',
-            'django-tastypie>=0.9.11',
-            'pyes>=0.19.1',
-        ),
-    )
+setup(
+    name = 'django-tastypie-elasticsearch',
+    version = '0.3.0',
+    description = "ElasticSearch support for django-tastypie.",
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    author = 'Jordi Llonch',
+    author_email = 'llonchj@gmail.com',
+    url = 'https://github.com/llonchj/django-tastypie-elasticsearch',
+    keywords = "REST RESTful tastypie pyes elasticsearch django",
+    license = 'AGPLv3',
+    packages = find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
+    classifiers = (
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ),
+    zip_safe = True,
+    install_requires = (
+        'Django>=1.4',
+        'django-tastypie>=0.9.11',
+        'elasticsearch',
+    ),
+    test_suite = 'tests.runtests.runtests',
+)
