1d7bb5514ae4bf6904964489464586ef135e995f
Jordi Llonch <llonchj@gmail.com>
2013-09-07 09:06:34 +1000

    moved requirements to external file

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
 import os
+import re
 from setuptools import setup, find_packages
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.test_project.settings")
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            # TODO support version numbers
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
 
 setup(
     name = 'django-tastypie-elasticsearch',
@@ -26,10 +42,6 @@ setup(
         'Framework :: Django',
     ),
     zip_safe = True,
-    install_requires = (
-        'Django>=1.4',
-        'django-tastypie>=0.9.11',
-        'elasticsearch',
-    ),
-    test_suite = 'tests.runtests.runtests',
+    install_requires=parse_requirements('requirements.txt'),
+    tests_require=parse_requirements('requirements-test.txt'),
 )
